084180d2e189e5293f8f00d2313f10fdff4fa80b
lif706 <lifeng0608@outlook.com>
2023-10-24 09:10:06 +0800

    django

---
+++ test2/views.py
@@ -0,0 +1,11 @@
+
+
+# Create your views here.
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
